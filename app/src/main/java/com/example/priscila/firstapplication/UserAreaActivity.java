2f660639b27becd3318271add1aacebc25fc0dd1
teteretu <teteretu@yahoo.com>
2017-12-22 12:58:32 -0200

    Changes

---
+++ app/src/main/java/com/example/priscila/firstapplication/UserAreaActivity.java
@@ -0,0 +1,21 @@
+package com.example.priscila.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class UserAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_area);
+
+        final EditText etUserName = (EditText) findViewById(R.id.etUsername);
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final TextView welcomeMessage = (TextView) findViewById(R.id.tvWelcomeMsg);
+
+    }
+}
